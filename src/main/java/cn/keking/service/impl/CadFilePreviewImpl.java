193aa10f96a6df177ea9ee28ad31c9447e839ec5
yudonghui <yudonghui@51caixiang.com>
2023-06-26 15:22:46 +0800

    first

---
+++ src/main/java/cn/keking/service/impl/CadFilePreviewImpl.java
@@ -0,0 +1,70 @@
+package cn.keking.service.impl;
+
+import cn.keking.config.ConfigConstants;
+import cn.keking.model.FileAttribute;
+import cn.keking.model.ReturnResponse;
+import cn.keking.service.FilePreview;
+import cn.keking.utils.DownloadUtils;
+import cn.keking.service.FileHandlerService;
+import cn.keking.web.filter.BaseUrlFilter;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+
+import static cn.keking.service.impl.OfficeFilePreviewImpl.getPreviewType;
+
+/**
+ * @author chenjh
+ * @since 2019/11/21 14:28
+ */
+@Service
+public class CadFilePreviewImpl implements FilePreview {
+
+    private static final String OFFICE_PREVIEW_TYPE_IMAGE = "image";
+    private static final String OFFICE_PREVIEW_TYPE_ALL_IMAGES = "allImages";
+    private static final String FILE_DIR = ConfigConstants.getFileDir();
+
+    private final FileHandlerService fileHandlerService;
+    private final OtherFilePreviewImpl otherFilePreview;
+
+    public CadFilePreviewImpl(FileHandlerService fileHandlerService, OtherFilePreviewImpl otherFilePreview) {
+        this.fileHandlerService = fileHandlerService;
+        this.otherFilePreview = otherFilePreview;
+    }
+
+    @Override
+    public String filePreviewHandle(String url, Model model, FileAttribute fileAttribute) {
+        // 预览Type，参数传了就取参数的，没传取系统默认
+        String officePreviewType = fileAttribute.getOfficePreviewType() == null ? ConfigConstants.getOfficePreviewType() : fileAttribute.getOfficePreviewType();
+        String baseUrl = BaseUrlFilter.getBaseUrl();
+        String fileName = fileAttribute.getName();
+        String pdfName = fileName.substring(0, fileName.lastIndexOf(".") + 1) + "pdf";
+        String outFilePath = FILE_DIR + pdfName;
+        // 判断之前是否已转换过，如果转换过，直接返回，否则执行转换
+        if (!fileHandlerService.listConvertedFiles().containsKey(pdfName) || !ConfigConstants.isCacheEnabled()) {
+            String filePath;
+            ReturnResponse<String> response = DownloadUtils.downLoad(fileAttribute, null);
+            if (response.isFailure()) {
+                return otherFilePreview.notSupportedFile(model, fileAttribute, response.getMsg());
+            }
+            filePath = response.getContent();
+            if (StringUtils.hasText(outFilePath)) {
+                boolean convertResult = fileHandlerService.cadToPdf(filePath, outFilePath);
+                if (!convertResult) {
+                    return otherFilePreview.notSupportedFile(model, fileAttribute, "cad文件转换异常，请联系管理员");
+                }
+                if (ConfigConstants.isCacheEnabled()) {
+                    // 加入缓存
+                    fileHandlerService.addConvertedFile(pdfName, fileHandlerService.getRelativePath(outFilePath));
+                }
+            }
+        }
+        if (baseUrl != null && (OFFICE_PREVIEW_TYPE_IMAGE.equals(officePreviewType) || OFFICE_PREVIEW_TYPE_ALL_IMAGES.equals(officePreviewType))) {
+            return getPreviewType(model, fileAttribute, officePreviewType, baseUrl, pdfName, outFilePath, fileHandlerService, OFFICE_PREVIEW_TYPE_IMAGE,otherFilePreview);
+        }
+        model.addAttribute("pdfUrl", pdfName);
+        return PDF_FILE_PREVIEW_PAGE;
+    }
+
+
+}
